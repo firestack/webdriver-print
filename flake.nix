e578a6dfd86a23efe267a3a106c0d644d3d95b94
Kayla Firestack <dev@kaylafire.me>
2022-07-01 19:43:50 -0400

    feat(repo): rename package

--- flake.nix
+++ flake.nix
@@ -32,14 +32,14 @@
 
 				# Build the actual crate itself, reusing the dependency
 				# artifacts from above.
-				my-crate = craneLib.buildPackage {
+				cdp-print = craneLib.buildPackage {
 					inherit cargoArtifacts src;
 				};
 			in
 			{
 				checks = {
 					# Build the crate as part of `nix flake check` for convenience
-					inherit my-crate;
+					inherit cdp-print;
 
 					# Run clippy (and deny all warnings) on the crate source,
 					# again, resuing the dependency artifacts from above.
@@ -47,27 +47,27 @@
 					# Note that this is done as a separate derivation so that
 					# we can block the CI if there are issues here, but not
 					# prevent downstream consumers from building our crate by itself.
-					my-crate-clippy = craneLib.cargoClippy {
+					cdp-print-clippy = craneLib.cargoClippy {
 						inherit cargoArtifacts src;
 						cargoClippyExtraArgs = "-- --deny warnings";
 					};
 
 					# Check formatting
-					my-crate-fmt = craneLib.cargoFmt {
+					cdp-print-fmt = craneLib.cargoFmt {
 						inherit src;
 					};
 				} // lib.optionalAttrs (system == "x86_64-linux") {
 					# NB: cargo-tarpaulin only supports x86_64 systems
 					# Check code coverage (note: this will not upload coverage anywhere)
-					my-crate-coverage = craneLib.cargoTarpaulin {
+					cdp-print-coverage = craneLib.cargoTarpaulin {
 						inherit cargoArtifacts src;
 					};
 				};
 
-				packages.default = my-crate;
+				packages.default = cdp-print;
 
 				apps.default = flake-utils.lib.mkApp {
-					drv = my-crate;
+					drv = cdp-print;
 				};
 
 				devShells.default = pkgs.mkShell {
