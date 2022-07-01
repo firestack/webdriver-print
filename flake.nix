f6e5388f6e4fdca1ffd22cfd0adea03feb6d20b6
Kayla Firestack <dev@kaylafire.me>
2022-07-01 19:42:11 -0400

    fix(repo): convert to tabs

--- flake.nix
+++ flake.nix
@@ -1,83 +1,83 @@
 {
-  description = "Build a cargo project";
+	description = "Build a cargo project";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+		crane = {
+			url = "github:ipetkov/crane";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+		flake-utils.url = "github:numtide/flake-utils";
+	};
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+	outputs = { self, nixpkgs, crane, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
 
-        inherit (pkgs) lib;
+				inherit (pkgs) lib;
 
-        craneLib = crane.lib.${system};
-        src = ./.;
+				craneLib = crane.lib.${system};
+				src = ./.;
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly {
-          inherit src;
-        };
+				# Build *just* the cargo dependencies, so we can reuse
+				# all of that work (e.g. via cachix) when running in CI
+				cargoArtifacts = craneLib.buildDepsOnly {
+					inherit src;
+				};
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        my-crate = craneLib.buildPackage {
-          inherit cargoArtifacts src;
-        };
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit my-crate;
+				# Build the actual crate itself, reusing the dependency
+				# artifacts from above.
+				my-crate = craneLib.buildPackage {
+					inherit cargoArtifacts src;
+				};
+			in
+			{
+				checks = {
+					# Build the crate as part of `nix flake check` for convenience
+					inherit my-crate;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy {
-            inherit cargoArtifacts src;
-            cargoClippyExtraArgs = "-- --deny warnings";
-          };
+					# Run clippy (and deny all warnings) on the crate source,
+					# again, resuing the dependency artifacts from above.
+					#
+					# Note that this is done as a separate derivation so that
+					# we can block the CI if there are issues here, but not
+					# prevent downstream consumers from building our crate by itself.
+					my-crate-clippy = craneLib.cargoClippy {
+						inherit cargoArtifacts src;
+						cargoClippyExtraArgs = "-- --deny warnings";
+					};
 
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # NB: cargo-tarpaulin only supports x86_64 systems
-          # Check code coverage (note: this will not upload coverage anywhere)
-          my-crate-coverage = craneLib.cargoTarpaulin {
-            inherit cargoArtifacts src;
-          };
-        };
+					# Check formatting
+					my-crate-fmt = craneLib.cargoFmt {
+						inherit src;
+					};
+				} // lib.optionalAttrs (system == "x86_64-linux") {
+					# NB: cargo-tarpaulin only supports x86_64 systems
+					# Check code coverage (note: this will not upload coverage anywhere)
+					my-crate-coverage = craneLib.cargoTarpaulin {
+						inherit cargoArtifacts src;
+					};
+				};
 
-        packages.default = my-crate;
+				packages.default = my-crate;
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
-        };
+				apps.default = flake-utils.lib.mkApp {
+					drv = my-crate;
+				};
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.checks;
+				devShells.default = pkgs.mkShell {
+					inputsFrom = builtins.attrValues self.checks;
 
-          # Extra inputs can be added here
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
-          ];
-        };
-      });
+					# Extra inputs can be added here
+					nativeBuildInputs = with pkgs; [
+						cargo
+						rustc
+					];
+				};
+			});
 }
