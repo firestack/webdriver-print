d79201530dbd3f165224b86f2cc996fa3bed84c6
Kayla Firestack <dev@kaylafire.me>
2022-07-28 13:22:32 -0400

    feat(main): add imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::time::Duration;
+
+use ::wdp::{Result, Options, write_pdf};
+use webdriver::command::PrintParameters;
+use fantoccini::{ClientBuilder, wd::Capabilities};
+
 #[tokio::main]
 async fn main() -> Result<()> {
 	let options: Options = clap::Parser::parse();
