649b0d125d71b90199247a84b332dee309702bb4
Kayla Firestack <dev@kaylafire.me>
2022-07-28 13:11:40 -0400

    feat(lib): add `WDPrint` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use webdriver::command::PrintParameters;
 
 #[doc(hidden)]
 pub type Result<T, E = Box<dyn Error + Send + Sync + 'static>> = core::result::Result<T, E>;
+
+#[derive(Debug)]
+pub struct WDPrint {
+	parameters: PrintParameters
+}
+
+impl From<PrintParameters> for WDPrint
+{
+	fn from(parameters: PrintParameters) -> Self {
+		WDPrint { parameters }
+	}
+}
