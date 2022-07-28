b4344103c0772f400501fe2b4ad47a6206bef0cc
Kayla Firestack <dev@kaylafire.me>
2022-07-28 13:22:40 -0400

    feat(main): add webdriver capabilities

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,21 @@ use fantoccini::{ClientBuilder, wd::Capabilities};
 async fn main() -> Result<()> {
 	let options: Options = clap::Parser::parse();
 
+	let capabilities: Capabilities = {
+		let headless_arg = match options.headless { true => Some("-headless"), false => None }.unwrap_or("");
+		let ff_cap = serde_json::json!({
+			"args": [
+				headless_arg
+			]
+		});
+
+		[
+			("moz:firefoxOptions".to_string(), ff_cap)
+		].into_iter().collect()
+	};
 
 	let c = ClientBuilder::rustls()
+		.capabilities(capabilities)
 		.connect(options.webdriver_url.as_str())
 		.await?;
 
